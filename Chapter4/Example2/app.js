b7d13f08cd2b3c495bc7bb1031108816c67dcb5f
vivek13462 <vivek.13462@gmail.com>
2016-09-23 05:29:53 +0530

    javascript file

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,3 +1,4 @@
+/* jshint browser: true, jquery: true, camelcase: true, indent: 2, undef: true, quotmark: single, maxlen: 80, trailing: true, curly: true, eqeqeq: true, forin: true, immed: true, latedef: true, newcap: true, nonew: true, unused: true, strict: true */
 var main = function () {
     "use strict";
 
@@ -12,6 +13,22 @@ var main = function () {
             $(".comment-input input").val("");
         }
     };
+    
+    
+    
+    var addCommentFromInputBoxuser2 = function () {
+        var $new_comment;
+
+        if ($(".comment-inputuser2 input").val() !== "") {
+            $new_comment = $("<p>").text($(".comment-inputuser2 input").val());
+            $new_comment.hide();
+            $(".commentsuser2").append($new_comment);
+            $new_comment.fadeIn();
+            $(".comment-inputuser2 input").val("");
+        }
+    };
+    
+    
 
     $(".comment-input button").on("click", function (event) {
         addCommentFromInputBox();
@@ -19,9 +36,22 @@ var main = function () {
 
     $(".comment-input input").on("keypress", function (event) {
         if (event.keyCode === 13) {
-            addCommentFromInputBox();
+          addCommentFromInputBox();
         }
     });
+    
+    
+        $(".comment-inputuser2 button").on("click", function (event) {
+        addCommentFromInputBoxuser2();
+    });
+
+    $(".comment-inputuser2 input").on("keypress", function (event) {
+        if (event.keyCode === 13) {
+          addCommentFromInputBoxuser2();
+        }
+    });
+    
+    
 };
 
 $(document).ready(main);
